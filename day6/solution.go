8816333e4cd2c60ada217d3ffc46fb21c1e10210
Robin de Boer <robindboer@gmail.com>
2022-12-13 19:35:32 +0100

    Add solution to day 6 using map

---
+++ day6/solution.go
@@ -0,0 +1,27 @@
+package main
+
+import (
+	"fmt"
+	"os"
+	"strings"
+)
+
+func main() {
+	input, _ := os.ReadFile("input.txt")
+	data := strings.TrimSpace(string(input))
+	fmt.Println(findMarker(data, 4))
+	fmt.Println(findMarker(data, 14))
+}
+
+func findMarker(characters string, markerSize int) int {
+	for i := markerSize; i <= len(characters); i++ {
+		m := map[rune]bool{}
+		for _, r := range characters[i-markerSize : i] {
+			m[r] = true
+		}
+		if len(m) >= markerSize {
+			return i
+		}
+	}
+	return -1
+}
